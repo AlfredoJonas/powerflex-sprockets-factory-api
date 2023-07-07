f72b92a7d6f4bfc70a33afa6d0c1adfd87455e37
Jonas Gonzalez <alfredojonas94@gmail.com>
2023-07-07 17:03:15 -0400

    Update var names to match AWS ENV deployment

--- app/settings.py
+++ app/settings.py
@@ -110,11 +110,11 @@ WSGI_APPLICATION = "app.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USERNAME"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOSTNAME", "postgres-db"),
-        "PORT": os.environ.get("DB_PORT", 5432),
+        "NAME": os.environ.get("RDS_DB_NAME"),
+        "USER": os.environ.get("RDS_USERNAME"),
+        "PASSWORD": os.environ.get("RDS_PASSWORD"),
+        "HOST": os.environ.get("RDS_HOSTNAME", "postgres-db"),
+        "PORT": os.environ.get("RDS_PORT", 5432),
     }
 }
 
