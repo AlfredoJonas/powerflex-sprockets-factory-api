65cb76e1576af5a0715485c5e7dde07088c53050
Jonas Gonzalez <alfredojonas94@gmail.com>
2023-07-07 17:28:12 -0400

    Add host url to the cors config

--- app/settings.py
+++ app/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "testserver",
+    "powerflex-sprockets-factory-api-dev.us-east-1.elasticbeanstalk.com"
 ]
 
 
@@ -62,6 +63,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
+    "http://powerflex-sprockets-factory-api-dev.us-east-1.elasticbeanstalk.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
